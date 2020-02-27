1b8a3a95109b5df7dd1f44b9be5108789b724b28
Marc Theunissen <mtheunissen82@gmail.com>
2020-02-27 13:26:27 +0100

    Add some DB / ORM query code

--- laravel-dierdemo/app/Http/Controllers/HomeController.php
+++ laravel-dierdemo/app/Http/Controllers/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Dier;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -13,7 +15,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        // $this->middleware('auth');
     }
 
     /**
@@ -23,6 +25,18 @@ class HomeController extends Controller
      */
     public function index()
     {
+        echo "<pre>";
+        $result = DB::table('dier')->select('id')->where('type', 'like', '%a%')->get();
+
+        foreach ($result as $dier) {
+            $dierObject = Dier::find($dier->id);
+            var_dump($dierObject);
+        }
+
+        $katten = Dier::where('type', 'like', '%a%')->get();
+        var_dump($katten);
+
+        echo "</pre>";
         return view('home');
     }
 }

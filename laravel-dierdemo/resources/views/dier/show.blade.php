e5b4750b872c856e6964fb7cbefae53d9f12f409
Marc Theunissen <mtheunissen82@gmail.com>
2020-03-04 12:53:02 +0100

    Update soort in show template

--- laravel-dierdemo/resources/views/dier/show.blade.php
+++ laravel-dierdemo/resources/views/dier/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div>
     <h2>Dier detailpagina</h2>
-    <p>type: {{ $dier->type }}</p>
+    <p>soort: {{ $dier->soort->naam }}</p>
     <p>naam: {{ $dier->naam }}</p>
     <button onclick="window.history.back()">Terug naar vorige pagina</button><br>
     <a href="{{ route('dier.index') }}">Terug naar overzicht</a><br>
